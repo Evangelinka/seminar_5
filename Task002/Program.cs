78aeace8054e22ae5da3e01a9affd67c5ccbb062
«Kristina» <kristinabol1996@gmail.com>
2022-08-23 19:01:28 +0800

    2 задача

---
+++ Task002/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу замены элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SwapNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+    Console.Write($"{array[i] * (-1)} ");
+    }
+}
+
+int[] array = CreateArray(4, -9, 9);
+PrintArray(array);
+SwapNumbers(array);
