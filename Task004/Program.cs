16e740944a51fc7c9b32d42a689054f6e6ef5b02
«Kristina» <kristinabol1996@gmail.com>
2022-08-23 19:04:19 +0800

    4 задача, сколько элементов в диапазоне

---
+++ Task004/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество 
+// элементов массива, значения которых лежат в отрезке [10, 99].
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+void FindNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >=10 && array[i]<100)
+            count += 1;
+    }
+    Console.WriteLine($"Количество элементов, которые входят в данный диапазон: {count}");
+}
+
+int[] array = CreateArray(123, -50, 250);
+PrintArray(array);
+FindNumbers(array);
