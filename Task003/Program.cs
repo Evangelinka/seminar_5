40d7ccbc973edb147760be6466bdfc04eef29d06
«Kristina» <kristinabol1996@gmail.com>
2022-08-23 19:03:00 +0800

    3 задача, есть ли число в массиве

---
+++ Task003/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное
+// число в массиве.
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FindNumber(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+            {
+                Console.WriteLine("Да");
+                return;
+            }
+    }
+    Console.WriteLine("Нет");
+}
+int[] array = CreateArray(7, 0, 10);
+PrintArray(array);
+FindNumber(array, 5);
